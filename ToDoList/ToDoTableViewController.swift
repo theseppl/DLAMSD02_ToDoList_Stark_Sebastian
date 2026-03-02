ccc2b1cc2427b6e4ce24451b613fd5a7a011f91b
Sebastian Stark <sstark@gmx.net>
2026-03-02 21:54:20 +0100

    ToDoTableViewController implementiert

---
+++ ToDoList/ToDoTableViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ToDoTableViewController.swift
+//  ToDoList
+//
+//  Created by Sebastian Stark on 02.03.26.
+//
+
+import Foundation
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+    var toDos = [ToDo]()
+    
+    // Gibt die Anzahl an Objekten in der Sektion zurück.
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDos.count
+    }
+    
+    // Liefert eine konfigurierte UITableViewCell für die angefragte Zeile zurück.
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // Holt eine Zelle.
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCellIdentifier", for: indexPath)
+        
+        // Holt die Daten für die Zelle.
+        let toDo = toDos[indexPath.row]
+        
+        // Darstellung der Zelle erzeugen.
+        var content = cell.defaultContentConfiguration()
+        content.text = toDo.title
+        
+        // Zelle übernimmt erzeugte Darstellung.
+        cell.contentConfiguration = content
+        return cell
+    }
+}
