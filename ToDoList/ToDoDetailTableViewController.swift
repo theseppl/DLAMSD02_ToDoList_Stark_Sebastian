a20d4bcd00e9d6d8a2b5e288674314ec3ef542c7
Sebastian Stark <sstark@gmx.net>
2026-03-04 21:17:52 +0100

    ToDoDetailTableViewController implementiert

---
+++ ToDoList/ToDoDetailTableViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ToDoDetailTableViewController.swift
+//  ToDoList
+//
+//  Created by Sebastian Stark on 04.03.26.
+//
+
+import UIKit
+
+class ToDoDetailTableViewController: UITableViewController {
+    @IBOutlet var titleTextField: UITextField!
+    @IBOutlet var isCompleteButton: UIButton!
+    @IBOutlet var dueDateLabel: UILabel!
+    @IBOutlet var dueDateDatePicker: UIDatePicker!
+    @IBOutlet var notesTextView: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
