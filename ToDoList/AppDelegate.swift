9b2d5a9cbb2b4d75fbc546111800bb10ce197745
Sebastian Stark <sstark@gmx.net>
2026-03-14 07:39:34 +0100

    Kommentare AppDelegate optimiert

--- ToDoList/AppDelegate.swift
+++ ToDoList/AppDelegate.swift
@@ -7,22 +7,33 @@
 
 import UIKit
 
+// Einstiegspunkt der App.
+// Der AppDelegate reagiert auf globale Ereignisse wie App-Start,
+// Benachrichtigungen oder das Erstellen neuer UI-Szenen.
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
     
     
-    // Die beiden folgenden Funktionen sorgen dafür, dass die Errinnerungsmeldung auch dann erscheint,
-    // wenn die App geöffnet ist.
+    // MARK: - App Start
+    
+    // Wird aufgerufen, sobald die App gestartet wurde.
+    // Hier ist der den Notification-Delegate implementiert, damit Benachrichtigungen
+    // auch angezeigt werden, wenn die App im Vordergrund läuft.
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
 
+        // Der AppDelegate empfängt Benachrichtigungen, auch wenn die App geöffnet ist.
         UNUserNotificationCenter.current().delegate = self
         return true
     }
     
+    
+    // MARK: - Benachrichtigungen im Vordergrund
+    
+    // Wird aufgerufen, wenn eine Benachrichtigung eintrifft, während die App aktiv im Vordergrund ist.
     func userNotificationCenter(
         _ center: UNUserNotificationCenter,
         willPresent notification: UNNotification,
@@ -31,7 +42,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         completionHandler([.banner, .sound])
     }
  
-    // MARK: UISceneSession Lifecycle
+    
+    // MARK: - UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
